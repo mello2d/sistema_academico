703b6c4125220d32fd1a0149c910200aac33995b
Excel Avançado 2019.6 <39117022019.6@senacrjedu.local>
2024-07-08 19:39:31 -0300

    Modelo e formulario Curso, alterar e excluir cursos

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from myapp.forms import CursoForm
+
+from myapp.models import Curso
 
 # Create your views here.
+
+# Alterar curso
+
+
+def alterarCurso(request, id):
+    curso = Curso.objects.get(id=id)
+
+    if request.method == "POST":
+        form = CursoForm(request.POST, instance=curso)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cursos')
+
+    form = CursoForm(instance=curso)
+    return render(request, 'cursos/form_curso.html', {'form': form})
+
+# Excluir curso
+
+
+def excluirCurso(request, id):
+    curso = Curso.objects.get(id=id)
+
+    try:
+        curso.delete()
+
+    except:
+        pass
+
+    return redirect(request, 'listar_cursos')
